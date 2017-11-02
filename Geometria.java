39183b094aa05ddb7cf8ce9370e329c61622cf02
Jorge <jorgemaryeb11@gmail.com>
2017-11-02 13:20:38 +0100

    Commit 1 Commit inicial, creando el repositorio 	nuevo archivo: .gitignore 	nuevo archivo: Geometria.java 	nuevo archivo: TestGeometria.java

---
+++ Geometria.java
@@ -0,0 +1,16 @@
+public class Geometria {
+	
+	public static double calcularAreaCuadrado (double lado) {
+		
+		return lado * lado;
+		
+	}
+	
+	public static double calcularAreaRectangulo (double base, double altura) {
+		
+		return base * altura;
+		
+	}
+	
+}
+
