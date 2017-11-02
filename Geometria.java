1e361b79af500eeebe0e3295d2eead4520b6ee7d
Jorge <jorgemaryeb11@gmail.com>
2017-11-02 14:16:36 +0100

    Commit 1º de documentacion 	modificado:    Geometria.java
    	Añadida documentación de Geometria.java de todos los métodos

--- Geometria.java
+++ Geometria.java
@@ -1,6 +1,20 @@
+/** Clase Geometria
+ * la vamos a usar para realizar
+ * diversos cálculos de geometría
+ * @author Jorge
+ * @version 1.0
+*/
+
 public class Geometria {
 	
 	public static final double PI = 3.1416;
+	
+	/** Método calcularAreaCuadrado
+	 * que nos calcula el área de un cuadrado
+	 * dado el lado
+	 * @param lado de tipo double, lado que el lado que define al cuadrado 
+	 * @return área del cuadrado, de tipo double
+	*/	
 	
 	public static double calcularAreaCuadrado (double lado) {
 		
@@ -8,17 +22,41 @@ public class Geometria {
 		
 	}
 	
+	/** Método calcularAreaRectangulo
+	 * que nos calcula el área de un rectángulo
+	 * dada la base y la altura
+	 * @param base de tipo double, base del rectángulo 
+	 * @param altura de tipo double, base del rectángulo
+	 * @return área del rectángulo, de tipo double
+	*/
+	
 	public static double calcularAreaRectangulo (double base, double altura) {
 		
 		return base * altura;
 		
 	}
 	
+	/** Método calcularAreaTriángulo
+	 * que nos calcula el área de un triángulo
+	 * dada la base y la altura
+	 * @param base de tipo double, base del triángulo 
+	 * @param altura de tipo double, base del triángulo
+	 * @return área del triángulo, de tipo double
+	*/
+	
+	
 	public static double calcularAreaTriangulo (double base, double altura) {
 		
 		return (base * altura)/2.0;
 		
 	}
+	
+	/** Método calcularAreaCirculo
+	 * que nos calcula el área de un círculo
+	 * dado el radio
+	 * @param radio de tipo double, radio que define el círculo  
+	 * @return área del círculo, de tipo double
+	*/
 	
 	public static double calcularAreaCirculo (double radio) {
 		
