86f7c9bb1e7e126800f8a189460aa3d4ae6efaba
Jorge <jorgemaryeb11@gmail.com>
2017-11-02 13:52:25 +0100

    Commit 3 	modificado:    Geometria.java 	modificado:    TestGeometria.java

--- Geometria.java
+++ Geometria.java
@@ -1,5 +1,7 @@
 public class Geometria {
 	
+	public static final double PI = 3.1416;
+	
 	public static double calcularAreaCuadrado (double lado) {
 		
 		return lado * lado;
@@ -12,5 +14,17 @@ public class Geometria {
 		
 	}
 	
+	public static double calcularAreaTriangulo (double base, double altura) {
+		
+		return (base * altura)/2.0;
+		
+	}
+	
+	public static double calcularAreaCirculo (double radio) {
+		
+		return radio * radio * PI;
+		
+	}
 }
+
 
--- TestGeometria.java
+++ TestGeometria.java
@@ -7,6 +7,12 @@ public class TestGeometria {
 		
 		double areaRectangulo = Geometria.calcularAreaRectangulo(2.123, 1.233);
 		System.out.println("Área del rectángulo de base 2.123 y de altura 1.233 vale: " + areaRectangulo);
+		
+		double areaTriangulo = Geometria.calcularAreaTriangulo(2.123, 1.233);
+		System.out.println("Área del triangulo de base 2.123 y de altura 1.233 vale: " + areaTriangulo);
+		
+		double areaCirculo = Geometria.calcularAreaCirculo(2.123);
+		System.out.println("Área del circulo de radio 2.123 vale: " + areaCirculo);
 	
 	}
 }
